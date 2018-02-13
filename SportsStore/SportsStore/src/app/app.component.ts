fb86a446e8bbd44417dfdde9d541418cfbad42ae
Unknown <chanta595@gmail.com>
2018-02-13 23:56:22 +0400

    Chapter 7 [1]
    добавление функциональности + создание функционального модуля

--- SportsStore/SportsStore/src/app/app.component.ts
+++ SportsStore/SportsStore/src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: '<div class="bg-success p-1 text-sm-center"> This is SportsStore</div>'
+  template: '<store></store>'
 })
 export class AppComponent {}
--- SportsStore/SportsStore/src/app/model/product.model.ts
+++ SportsStore/SportsStore/src/app/model/product.model.ts
@@ -2,7 +2,7 @@ export class Product {
     constructor(
         public id?: number,
         public name?: string,
-        public category?: string;
-        public discription?: string,
+        public category?: string,
+        public description?: string,
         public price?: number){ }
 }
