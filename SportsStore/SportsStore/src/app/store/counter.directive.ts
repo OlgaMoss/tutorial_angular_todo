16cecb142c239279f5f0de9aa056aedf05972e80
Unknown <chanta595@gmail.com>
2018-02-15 01:14:25 +0400

    Chapter 7 [2] - end
    Создание пользовательских директив

---
+++ SportsStore/SportsStore/src/app/store/counter.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, Input, Attribute, SimpleChange, ViewContainerRef, TemplateRef}  from "@angular/core";
+
+
+
+@Directive({
+    selector:"[counterOf]"
+})
+export class CounterDirective{
+    constructor(private container:ViewContainerRef, private template: TemplateRef<Object>){}
+
+    @Input('counterOf') 
+    counter: number;
+
+    ngOnChanges(changes: SimpleChange) {
+        this.container.clear();
+        for (let i = 0; i < this.counter; i++) {
+            this.container.createEmbeddedView(this.template, new CounterDirectiveContex(i + 1));
+        }
+    }
+}
+
+class CounterDirectiveContex{
+    constructor(public $implicit: any){}
+}
