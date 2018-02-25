776d0419cf528128333ff738acac7f3311576d65
Unknown <chanta595@gmail.com>
2018-02-25 20:36:56 +0400

    Chapter 9[1]
    добавила расширение данных и репозиториев для админки

--- SportsStore/SportsStore/src/app/model/rest.datasource.ts
+++ SportsStore/SportsStore/src/app/model/rest.datasource.ts
@@ -35,8 +35,32 @@ export class RestDataSource {
         return this.sendRequest(RequestMethod.Get, 'products');
     }
 
+    saveProduct(product: Product): Observable<Product> {
+        return this.sendRequest(RequestMethod.Post, 'products', product, true);
+    }
+
+    updateProduct(product: Product): Observable<Product> {
+        return this.sendRequest(RequestMethod.Put, `products/${product.id}`, product, true);
+    }
+
+    deleteProduct(id: number): Observable<Product> {
+        return this.sendRequest(RequestMethod.Delete, `products/${id}`, null, true);
+    }
+
+    getOrders(): Observable<Order[]> {
+        return this.sendRequest(RequestMethod.Get, 'orders', null, true);
+    }
+
     saveOrder(order: Order): Observable<Order> {
-        return this.sendRequest(RequestMethod.Post, 'orders', order);
+        return this.sendRequest(RequestMethod.Post, 'orders', order) as Observable<Order>;
+    }
+
+    updateOrder(order: Order): Observable<Order> {
+        return this.sendRequest(RequestMethod.Put, `orders/${order.id}`, order, true) as Observable<Order>;
+    }
+
+    deleteOrder(id: number): Observable<Order> {
+        return this.sendRequest(RequestMethod.Delete, `orders/${id}`, null, true) as Observable<Order>;
     }
 
     // private sendRequest(verb: RequestMethod, url: string, body?: Product | Order): Observable<any> {
@@ -54,7 +78,7 @@ export class RestDataSource {
             body: body
         });
         if (auth && this.auth_token != null) {
-            request.headers.set('Authorization', `Bearer<${this.auth_token}>`);
+            request.headers.set('authorization', `Bearer<${this.auth_token}>`);
         }
         return this.http.request(request).map(response => response.json());
     }
