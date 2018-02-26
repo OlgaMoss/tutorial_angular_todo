5dbee97aefb7135b1bbeceab4afabfbc8c7fc284
Unknown <chanta595@gmail.com>
2018-02-26 23:50:23 +0400

    Chapter 10
    Docker, остались сложности

---
+++ SportsStore/SportsStore/deploy-server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var app = express();
+
+app.use("/node_modules",
+        express.static("/Users/chanta/myGitHub/tutorial_angular_todo/SportsStore/SportsStore/node_modules"));
+app.use("/",
+        express.static("/Users/chanta/myGitHub/tutorial_angular_todo/SportsStore/SportsStore/src/app"));
+
+app.listen(3000, function(){
+    console.log("HTTP Server running on port 3000");
+});
