c95db346ef4749ee167847f7b03cbfe260782b97
Unknown <chanta595@gmail.com>
2018-06-30 19:32:37 +0400

    Chapter 14

--- example/app/component.ts
+++ example/app/component.ts
@@ -1,6 +1,8 @@
 ﻿import { Component, ApplicationRef } from "@angular/core";
+import { NgForm } from '@angular/forms';
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
+import { ProductFormGroup } from "./form.model"
 
 @Component({
     selector: "app",
@@ -8,21 +10,8 @@ import { Product } from "./product.model";
 })
 export class ProductComponent {
     model: Model = new Model();
-
-    constructor(ref: ApplicationRef){
-        (<any>window).appRef = ref;
-        (<any>window).model = this.model;
-    }
-
-    getProductByPosition(position: number): Product{
-        return this.model.getProducts()[position];
-    }
-
-    getClassesByPosition(position: number): string{
-        let product = this.model.getProducts()[position];
-        return "p-a-1 "+(product.price < 50 ? "bg-info" : "bg-warning");
-    }
-
+    form: ProductFormGroup = new ProductFormGroup();
+  
     getProduct(key: number): Product{
         return this.model.getProduct(key);
     }
@@ -31,20 +20,60 @@ export class ProductComponent {
         return this.model.getProducts();
     }
 
-    getProductCount(): number{
-        console.log("getProductCount invoked");
-        return this.model.getProducts().length;
+    selectedProduct: string;
+    getSelected(product: Product): boolean{
+        return product.name == this.selectedProduct;
     }
 
-    targetName: string = "Kayak";
+    newProduct: Product = new Product();
 
-    counter: number = 1;
-
-    get nextProduct(): Product{
-        return this.model.getProducts().shift();
+    get jsonProduct(){
+        return JSON.stringify(this.newProduct)
     }
 
-    getKey(index: number, product: Product){
-        return product.category;
+    addProduct(p: Product){
+        console.log("New Product: "+ this.jsonProduct)
+    }
+
+    getValidationMessages(state: any, thingName: string){
+        let thing: string = state.path || thingName;
+        let messages: string[] =[];
+        if(state.errors){
+            for (let errorName in state.errors){
+                switch(errorName){
+                    case "required":
+                    messages.push (`You must enter a ${thing}`);
+                    break;
+                    case "minlength":
+                    messages.push(`A ${thing} must be at least ${state.errors['minlength'].requiredLength} characters`);
+                    break;
+                    case "pattern":
+                    messages.push(`The ${thing} contains illegal characters`);
+                    break;
+                }
+            }
+        }
+        return messages;
+    }
+
+    formSubmitted: boolean = false;
+
+    submitForm(form: NgForm){
+        this.formSubmitted = true;
+        if(form.valid){
+            this.addProduct(this.newProduct);
+            this.newProduct = new Product();
+            form.reset();
+            this.formSubmitted = false;
+        }
+    }
+
+    getFormValidationMessages(form: NgForm): string[] {
+       let messages: string[] = [];
+       Object.keys(form.controls).forEach( k=> {
+          this.getValidationMessages(form.controls[k], k)
+            .forEach(m => messages.push(m));
+       });
+       return messages;
     }
 }
